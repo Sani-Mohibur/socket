5bd2326e1ffc958ee75262588a1440995bbcd575
Sani-Mohibur <mohiburrahmansani@gmail.com>
2026-02-27 15:15:43 +0600

    cleanup

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { AuthRoutes } from '../modules/auth/auth.route';
 import { UserRoutes } from '../modules/user/user.route';
+import { ChatRoutes } from '../modules/chat/chat.route';
 
 const router = Router();
 
@@ -12,6 +13,10 @@ const moduleRoutes = [
   {
     path: '/user',
     route: UserRoutes,
+  },
+  {
+    path: '/chat',
+    route: ChatRoutes,
   },
 ];
 
